78886ebaf99b0c956da4428277d013928d7ef2c7
Jonathan Guthrie <jono@perfect.org>
2016-10-17 11:13:06 -0400

    update package name to remove ambiguity

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
 import PackageDescription
 
 let package = Package(
-	name: "Perfect-CURL",
+	name: "Perfect-CURL-Example",
 	targets: [],
 	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2, minor: 0)
 	]
